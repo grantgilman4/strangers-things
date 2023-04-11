8509c405826bfb4398e4f75222efdaf834a74867
grantGilman <grantgilman4@gmail.com>
2023-04-11 10:56:21 -0400

    adds react structure

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { ReactDOM } from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const domNode = document.querySelector('#root');
+const root = React.createRoot(domNode);
+root.render(<></>)
